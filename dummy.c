fa8caa01aa900cf9a0424bba83af8a63bc59152e
DraxTube01 <draxtube01@gmail.com>
2026-02-27 14:48:00 +0100

    Update dummy.c

--- dummy.c
+++ dummy.c
@@ -1,4 +1,4 @@
-// dummy.c - Minimal stub implementations for PSP port (no headers)
+// dummy.c - Stub implementations for PSP port
 
 typedef int boolean;
 typedef unsigned char byte;
@@ -10,9 +10,15 @@ typedef struct sfxinfo_s sfxinfo_t;
 struct wbstartstruct_s;
 typedef struct wbstartstruct_s wbstartstruct_t;
 
+// Global variables
 int snd_musicdevice = 0;
 int vanilla_keyboard_mapping = 1;
 
+// Network variables (NEW - fixes d_loop.c and d_main.c)
+boolean drone = false;
+boolean net_client_connected = false;
+
+// Sound
 boolean I_SoundIsPlaying(int handle) { return false; }
 int I_GetSfxLumpNum(sfxinfo_t *sfx) { return 0; }
 int I_StartSound(sfxinfo_t *sfxinfo, int channel, int vol, int sep, int pitch) { return 0; }
@@ -24,6 +30,7 @@ void I_ShutdownSound(void) {}
 void I_PrecacheSounds(sfxinfo_t *sounds, int num_sounds) {}
 void I_BindSoundVariables(void) {}
 
+// Music
 void I_InitMusic(void) {}
 void I_ShutdownMusic(void) {}
 void I_SetMusicVolume(int volume) {}
@@ -35,12 +42,15 @@ void I_PlaySong(void *handle, boolean looping) {}
 void I_StopSong(void) {}
 boolean I_MusicIsPlaying(void) { return false; }
 
+// Joystick
 void I_InitJoystick(void) {}
 void I_BindJoystickVariables(void) {}
 
+// Input
 void I_InitInput(void) {}
 void I_GetEvent(void) {}
 
+// Misc
 void I_Endoom(byte *endoom_data) {}
 void StatCopy(wbstartstruct_t *stats) {}
 void StatDump(void) {}
